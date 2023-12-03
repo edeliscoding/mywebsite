53f56ae08286a70efda440a0e76b609b77717ee4
edeliscoding <edelreichd@gmail.com>
2023-12-03 16:14:23 -0700

    changed signin text color

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -89,7 +89,7 @@ export default function Login() {
 
               <button
                 type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border border-slate-500"
+                className="w-full text-teal-500 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border border-slate-500"
               >
                 Sign in
               </button>
